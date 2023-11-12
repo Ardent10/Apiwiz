d67bd039398a377651b38d4df548d8198b2e4758
Zaky <zak786khan@gmail.com>
2023-11-12 13:35:47 +0530

    Feat: APIWIZ

---
+++ public/config.js
@@ -0,0 +1,6 @@
+window.env = {
+    PUBLIC_KEY : "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCzcCO2EWrEaZfPg2kR+9UTD5sOWt0gcT+B2+pSkhxBYa2RaKb1y3FNL1ZJA3PQ0fdgePt1CIqUIe1ZB+bu6IvdMiwpUYqyDwHfKj3o9aUza6a0hlZCCSdKtLlnaxjINmdskrtIZubd+HfSsrBUE0+QxT7ke3m/l98Yl6oBNyhW0QIDAQAB",
+    GOOGLE_OAUTH_CLIENT_ID:"<google_auth_client_id>",
+    API_BASE_URL:'http://localhost:3002/',
+    BASE_URL: 'http://localhost:3000/',
+}
